7033d1ccb1640e561fc388506bb32d11b0aef569
hasret <rabiamengi1@hotmail.com>
2023-09-25 12:31:41 -0400

    Cleanup unused code

--- WantedRobots/Data/AgentData.cs
+++ WantedRobots/Data/AgentData.cs
@@ -5,6 +5,9 @@ namespace WantedRobots.Models;
 public class AgentData : IAgentData
 {
     private int _nextId = 4;
+
+     // Initialisez la liste AgentRobotLinks dans le constructeur
+   
     public List<Agent> Agents { get; } = new List<Agent>
         {
             new Agent
@@ -37,44 +40,9 @@ public class AgentData : IAgentData
         _nextId++;
     }
 
-   /* public Agent GetAgentById(int idAgent)
-    {
-        // Recherchez le agent par son ID dans la liste
-        var agent = Agents.FirstOrDefault(r => r.Id == idAgent);
-        return agent;
-    }
-    public Agent GetAgentByName(string nomAgent)
-    {
-        var agent = Agents.FirstOrDefault(r => r.Nom == nomAgent);
-        return agent;
-    }*
+    
 
-    /* public void UpdateAgentPays(int idRobot, string nouveauPays)
-     {
-         // Recherchez le agent par son ID dans la liste
-         var agent = Agents.FirstOrDefault(r => r.Id == idRobot);
-         if (agent != null)
-         {
-             agent.Pays = nouveauPays;
-         }
-     }
 
-     public void DeleteRobot(int idAgent)
-     {
-         // Recherchez le agent par son ID dans la liste
-         var agent = Agents.FirstOrDefault(r => r.Id == idAgent);
-
-         if (agent != null)
-         {
-             // Supprimez le agent de la liste
-             Agents.Remove(agent);
-
-         }
-     }*/
-    /*public List<Agent> GetRobotsByCountry(string country)
-    {
-        return Agents.Where(r => string.Equals(r.Zone, country, StringComparison.OrdinalIgnoreCase)).ToList();
-    }*/
 
 
 }
